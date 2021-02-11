56b95dc42f6883a79b7fc8065668ddc2ac18f59a
DenizardDimitri-Camargo <62820018+DenizardDimitri-Camargo@users.noreply.github.com>
2021-02-10 20:57:08 -0400

    First Commit
    Criar projeto

---
+++ VimeoSample/VimeoSample/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VimeoSample.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
